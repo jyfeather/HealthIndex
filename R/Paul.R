1aa14e8995a378444f53c670a201c5a8352fae8c
Yan <jyfeather@gmail.com>
2014-12-23 09:38:40 +0800

    minor

--- R/Paul.R
+++ R/Paul.R
@@ -12,9 +12,9 @@ if (FALSE) {
   cols <- strtrim(cols, nchar(cols)-1)
   lapply(1:55, function(x) identical(cols[cols.1[x]], cols[cols.2[x]]))
   lapply(1:55, function(x) identical(cols[cols.2[x]], cols[cols.3[x]]))
-  check.3 <- unlist(lapply(1:nrow(dat.cor), function(x) any(is.na(dat.cor[x,]))))
-  table(check.3)
-  dat.3 <- dat.cor[which(!check.3),]
+  check.2 <- unlist(lapply(1:nrow(dat.cor), function(x) any(is.na(dat.cor[x,1:111]))))
+  table(check.2)
+  dat.2 <- dat.cor[which(!check.2),]
   # save .RData
   unlink("Paul.RData")
   save.image(file = "Paul.RData")
@@ -26,9 +26,9 @@ load(file = "./Paul.RData")
 #######################################################################
 #                         Quadratic Formulation  
 #######################################################################
-tot.1 <- dat.3[,2:56]
-tot.2 <- dat.3[,57:111]
-tot.3 <- dat.3[,112:166]
+tot.1 <- dat.2[,2:56]
+tot.2 <- dat.2[,57:111]
+tot.3 <- dat.2[,112:166]
 # standardization
 library(matrixStats)
 mean.1 <- colMeans(tot.1)
@@ -38,7 +38,7 @@ for (i in 1:55) {
   tot.2[,i] = (tot.2[,i]-mean.1[i])/sd.1[i]
   tot.3[,i] = (tot.3[,i]-mean.1[i])/sd.1[i]
 }
-num.sub <- nrow(dat.3)
+num.sub <- nrow(dat.2)
 num.epo <- 3 # num of epochs is 3
 num.cor <- 55
 train.no <- sample(num.sub, round(2/3*num.sub))
@@ -47,17 +47,20 @@ train.2 <- tot.2[train.no,]
 train.3 <- tot.3[train.no,]
 dat.no <- c(1:num.sub)
 test.no <- dat.no[!dat.no %in% train.no]
-test.sub <- dat.3[test.no, 1]
+test.sub <- dat.2[test.no, 1]
 test.1 <- tot.1[test.no,]
 test.2 <- tot.2[test.no,]
 test.3 <- tot.3[test.no,]
 
-#library(quadprog)
+omega.name <- colnames(dat.2)[-1] 
+omega.name <- substr(omega.name, 3, nchar(omega.name) - 2)
+
 e1 <- train.2 - train.1
 e2 <- train.3 - train.2
 e1 <- setNames(e1, 1:55)
 e2 <- setNames(e2, 1:55)
 E <- rbind(e1, e2)
+E <- na.omit(E)
 
 #######################################################################
 #                         Use AMPL to solve this problem  
@@ -79,42 +82,65 @@ ind.1 <- as.matrix(test.1) %*% omega$V1
 ind.2 <- as.matrix(test.2) %*% omega$V1
 ind.3 <- as.matrix(test.3) %*% omega$V1
 
-dat.viz <- rbind(cbind(test.sub, index = ind.1, time = rep(1, 41)), 
-      cbind(test.sub, index = ind.2, time = rep(2, 41)), 
-      cbind(test.sub, index = ind.3, time = rep(3, 41)))
-rownames(dat.viz) <- NULL
-dat.viz <- as.data.frame(dat.viz)
-as.factor(dat.viz$time)
+viz.idx <- rbind(cbind(test.sub, index = ind.1, time = rep(1, length(test.no))), 
+      cbind(test.sub, index = ind.2, time = rep(2, length(test.no))), 
+      cbind(test.sub, index = ind.3, time = rep(3, length(test.no))))
+rownames(viz.idx) <- NULL
+viz.idx <- as.data.frame(viz.idx)
+as.factor(viz.idx$time)
+viz.idx$no <- rep(1:length(test.no),3)
+viz.idx$time <- as.factor(viz.idx$time)
 
 library(ggplot2)
 # trend plot, curve
-ggplot(data = dat.viz, aes(x=time, y=V2, group = test.sub)) + geom_point() + geom_line() +
+ggplot(data = na.omit(viz.idx[viz.idx$no %in% 1:25, ]), aes(x=time, y=V2, group = test.sub)) + geom_point() + geom_line() +
   scale_y_continuous(name = "Health Index Value") +
-  scale_x_discrete(labels = c("BL", "6", "12")) + facet_grid(.~test.sub)
+  scale_x_discrete(labels = c("BL", "6", "12")) + facet_grid(.~test.sub) +
+  ggtitle("Health Index on Testing Set")
 
 # trend plot, dot
-ggplot(data = dat.viz, aes(x = value, y = no, colour = valuable)) +
-  geom_point(aes(x = V3, col = "1")) +
-  geom_point(aes(x = V4, col = "2")) +
-  geom_point(aes(x = V5, col = "3")) +
-  scale_color_manual(values = c("#FFCC00", "#0033CC", "#CC0033"), breaks = c("1", "2", "3"), 
-                     labels = c("Time 1", "Time 2", "Time 3")) +
+ggplot(data = na.omit(viz.idx), aes(x = V2, y = no, group = time, color = time)) +
+  geom_point() +
+  scale_color_manual(values = c("#FFCC00", "#0033CC", "#CC0033"), labels = c("Time 1", "Time 2", "Time 3")) +
   scale_x_continuous(name="Health Index Value") +
-  scale_y_discrete(name="Subject ID", breaks = c(1:length(test.no)), labels = dat.viz$id) +
+  scale_y_discrete(name="Subject ID", breaks = c(1:length(test.no)), labels = test.sub) +
   ggtitle("Health Index on Testing Set")
 
 # variable trend plot, curve
-mean <- colMeans(dat.3[,-1])
-omega.name <- colnames(dat.3)[-1] 
-omega.name <- substr(omega.name, 3, nchar(omega.name) - 2)
-mean.1 <- mean[31:55]
-mean.2 <- mean[86:110]
-mean.3 <- mean[141:165]
-dat.mean <- rbind(cbind(mean.1, time = rep(1,25)),
-                  cbind(mean.2, time = rep(2,25)),
-                  cbind(mean.3, time = rep(3,25)))
+no.var = 2 
+viz.var <- as.data.frame(c(dat.2[,no.var+1], dat.2[,no.var+56], dat.2[,no.var+111]))
+viz.var$time <- c(rep(1,nrow(viz.var)/3), rep(2, nrow(viz.var)/3), rep(3, nrow(viz.var)/3))
+viz.var$group <- rep(1:as.integer(nrow(viz.var)/3), 3)
+colnames(viz.var) <- c("variable", "time", "group")
+viz.var2 <- c(mean(viz.var[viz.var$time == 1,1]), 1)
+viz.var2 <- rbind(viz.var2, c(mean(viz.var[viz.var$time == 2,1]), 2))
+viz.var2 <- rbind(viz.var2, c(mean(viz.var[viz.var$time == 3,1]), 3))
+viz.var2 <- as.data.frame(viz.var2)
+rownames(viz.var2) <- NULL
+viz.var3 <- as.data.frame(cbind(dat.2[,no.var + 1], dat.2[,no.var+56], dat.2[,no.var+111]))
+source("./R/TrendStat.R")
+diff.var3 <- diff2(viz.var3)
+check.var3 <- as.data.frame(check(diff.var3))
+ggplot() + 
+  geom_point(data = viz.var, aes(x=time, y=variable, group = group)) +
+  geom_line(data = viz.var, aes(x=time, y=variable, group = group)) + 
+  geom_point(data = viz.var2, aes(x=V2,y=V1), colour = "red") +
+  geom_line(data = viz.var2, aes(x=V2,y=V1), colour = "red") +
+  ggtitle(omega.name[no.var]) +
+  annotate("text", x = 2.5, y = 0.75, label = paste("Strong Mono = ", check.var3[1,2], 
+                                                   "\nMono = ", check.var3[2,2],
+                                                   "\nAgainst Mono = ", check.var3[3,2]))
+
+# mean of variable trend plot, curve
+mean <- colMeans(dat.2[train.no,-1])
+mean.1 <- mean[1:30]
+mean.2 <- mean[56:85]
+mean.3 <- mean[111:140]
+dat.mean <- rbind(cbind(mean.1, time = rep(1,30)),
+                  cbind(mean.2, time = rep(2,30)),
+                  cbind(mean.3, time = rep(3,30)))
 dat.mean <- as.data.frame(dat.mean)
-dat.mean <- cbind(name = rep(omega.name[31:55], 3), dat.mean)
+dat.mean <- cbind(name = rep(omega.name[1:30], 3), dat.mean)
 rownames(dat.mean) <- NULL
 as.factor(dat.mean$time)
 ggplot(data = dat.mean, aes(x=time, y=mean.1, group = name)) + geom_point() + geom_line() +
--- R/TrendStat.R
+++ R/TrendStat.R
@@ -1,21 +1,7 @@
-#rm(list = ls())
-
-idx <- read.csv(file = "./data/tmp/idx.csv")
-tot.idx <- idx[,c(-1,-2,-6)]
-ad.idx <- idx[idx$group == "AD",c(-1,-2,-6)]
-mci.idx <- idx[idx$group == "MCI",c(-1,-2,-6)]
-nc.idx <- idx[idx$group == "NC",c(-1,-2,-6)]
-
-#######################################################################
-#                         Monotone Check  
-#######################################################################
 diff2 <- function(df) {
   return(cbind(df[,2]-df[,1], df[,3]-df[,2], df[,3]-df[,1]))
 }
-ad.dif <- diff2(ad.idx)
-mci.dif <- diff2(mci.idx)
-nc.dif <- diff2(nc.idx)
-tot.dif <- diff2(tot.idx)
+
 # Level 1: bl - m06 - m12  + + +
 # Level 2: bl - m12 - m06  + - +
 #          m06 - bl - m12  - + +
@@ -36,16 +22,3 @@ check <- function(dif) {
   })
   return(table(extent))
 }
-ad.check <- as.data.frame(check(ad.dif))
-mci.check <- as.data.frame(check(mci.dif))
-nc.check <- as.data.frame(check(nc.dif))
-tot.check <- as.data.frame(check(tot.dif))
-
-
-dat.viz <- as.data.frame(c(ad.check$Freq, mci.check$Freq, nc.check$Freq, tot.check$Freq))
-dat.viz <- cbind(freq = dat.viz[,1], trend = as.factor(rep(c("Strong Mono", "Mono", "Against Mono"),4)))
-dat.viz <- cbind(stage = c(rep("AD",3), rep("MCI",3), rep("NC",3), rep("TOTAL",3)), dat.viz)
-dat.viz <- as.data.frame(dat.viz)
-library(ggplot2)
-ggplot(data = dat.viz) +
-  geom_bar(aes(x = stage, fill = trend), position = "dodge", stat = "bin")
