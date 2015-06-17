5ab96dd85ce0f699723bf470d4d3898ca8979844
Yan Jin <jyfeather@gmail.com>
2015-06-17 10:49:35 -0700

    update

--- R/HealthIndex.R
+++ R/HealthIndex.R
@@ -1,9 +1,100 @@
-###################################
-# kernel, similar to one class SVM
+library(quadprog) # this is a quadratic problem
+
+#################################################
+# using kernel trick, support
 # - linear
-# - Guassian
-# - Polynomial
-###################################
-healthindex <- function() {
+# - gaussian
+# - polynomial
+#################################################
+kernel <- function(x, y, type = "linear") {
+  switch(type,
+         linear     =  x%*%y,
+         gaussian   =  exp(-sum((x-y)^2)/2),
+         polynomial =  (x%*%y+1)^2)
+}
+
+#################################################
+## compute the kernel matrix in the Lagrange dual
+## computeK = k(x2,y2)-k(x1,y1)-k(x2,y1)+k(x1,y1)
+#################################################
+computeK <- function(x1, x2, y1, y2, type = "linear") {
+  return(kernel(x1,y1,type)-kernel(x1,y2,type)-kernel(x2,y1,type)+kernel(x2,y2,type))  
+}
+
+#################################################
+## merge dataset by observations
+#################################################
+merge.all <- function(x, ..., by = "row.names") {
+  L <- list(...)
+  for (i in seq_along(L)) {
+    x <- merge(x, L[[i]], by = by)
+    rownames(x) <- x$Row.names
+    x$Row.names <- NULL
+  }
+  return(x)
+}
+
+#################################################
+## construct D
+## Currently, num.epoch is euqal
+#################################################
+Kmat <- function(data, num.obs, num.epoch, num.var, type = "linear") {
+  data <- as.matrix(data)
+  num <- num.obs*(num.epoch-1)
+  Dmat <- matrix(NA, nrow = num, ncol = num)
+  for(i in 1:num) {
+    # generate x1, x2
+    x.i <- (i+1)%/%(num.epoch-1) # current subject
+    x.j <- (i-1)%%(num.epoch-1)    # current time
+    x1 <- data[x.i, (x.j*num.var+1):(x.j*num.var+num.var)]  
+    x2 <- data[x.i, ((x.j+1)*num.var+1):((x.j+1)*num.var+num.var)]  
+    # generate y1, y2  
+    for(j in 1:i) {
+      y.i <- (j+1)%/%(num.epoch-1)
+      y.j <- (j-1)%%(num.epoch-1)
+      y1 <- data[y.i, (y.j*num.var+1):(y.j*num.var+num.var)]  
+      y2 <- data[y.i, ((y.j+1)*num.var+1):((y.j+1)*num.var+num.var)]      
+      Dmat[i,j] <- computeK(x1, x2, y1, y2, type)
+      Dmat[j,i] <- Dmat[i,j]
+    }
+  }
+  return(Dmat)
+}
+
+#################################################
+## C should be negavtive, determined by CV
+#################################################
+solveHI <- function(data, num.obs, num.epoch, num.var, C, type = "linear") {
+  num <- num.obs*(num.epoch-1)
+  Dmat <- Kmat(data, num.obs, num.epoch, num.var, type) 
+  dvec <- rep(1, num)
+  bvec <- c(rep(0, num), rep(C, num))
+  Amat <- rbind(diag(num), -diag(num))
   
+  # find nearest positive definite matrix since Dmat is not always PD
+  require(Matrix)
+  Dmat_PD <- as.matrix(nearPD(Dmat)$mat)
+  
+  result <- solve.QP(Dmat = Dmat_PD, dvec = dvec, Amat = t(Amat), bvec = bvec)
+  return(result$solution)  
+}
+
+#################################################
+## test is one observation
+#################################################
+computeHI <- function(train, test, coef, num.obs, num.epoch, num.var, type = "linear") {
+  train <- as.matrix(train)
+  test <- as.matrix(test)
+  num <- num.obs*(num.epoch-1)  
+  index <- 0
+  for(i in 1:num) {
+    # generate x1, x2
+    x.i <- (i+1)%/%(num.epoch-1) # current subject
+    x.j <- (i-1)%%(num.epoch-1)    # current time
+    x1 <- train[x.i, (x.j*num.var+1):(x.j*num.var+num.var)]  
+    x2 <- train[x.i, ((x.j+1)*num.var+1):((x.j+1)*num.var+num.var)]  
+    
+    index <- index + coef[i]*(kernel(x2,test)-kernel(x1,test))  
+  }
+  return(index)
 }
--- example/ADNI.R
+++ example/ADNI.R
@@ -8,18 +8,18 @@ if (FALSE) {
   ad.bs <- read.xlsx(file = "./data/ADNI/ADNIaalBM_BSL_AD.xls", header = TRUE, sheetIndex = 1)
   ad.m6 <- read.xlsx(file = "./data/ADNI/ADNIaalBM_M06_AD.xls", header = TRUE, sheetIndex = 1)
   ad.m12 <- read.xlsx(file = "./data/ADNI/ADNIaalBM_M12_AD.xls", header = TRUE, sheetIndex = 1)
-  mci.bs <- read.xlsx(file = "./data/ADNI/ADNIaalBM_BSL_MCI.xls", header = TRUE, sheetIndex = 1)
-  mci.m6 <- read.xlsx(file = "./data/ADNI/ADNIaalBM_M06_MCI.xls", header = TRUE, sheetIndex = 1)
-  mci.m12 <- read.xlsx(file = "./data/ADNI/ADNIaalBM_M12_MCI.xls", header = TRUE, sheetIndex = 1)
+#  mci.bs <- read.xlsx(file = "./data/ADNI/ADNIaalBM_BSL_MCI.xls", header = TRUE, sheetIndex = 1)
+#  mci.m6 <- read.xlsx(file = "./data/ADNI/ADNIaalBM_M06_MCI.xls", header = TRUE, sheetIndex = 1)
+#  mci.m12 <- read.xlsx(file = "./data/ADNI/ADNIaalBM_M12_MCI.xls", header = TRUE, sheetIndex = 1)
   nl.bs <- read.xlsx(file = "./data/ADNI/ADNIaalBM_BSL_NL.xls", header = TRUE, sheetIndex = 1)
   nl.m6 <- read.xlsx(file = "./data/ADNI/ADNIaalBM_M06_NL.xls", header = TRUE, sheetIndex = 1)
   nl.m12 <- read.xlsx(file = "./data/ADNI/ADNIaalBM_M12_NL.xls", header = TRUE, sheetIndex = 1)
   #ad.bs[,1] == ad.m6[,1] # check ID consistency
   # Whole dataset
-  dat.bs <- rbind(ad.bs[,c(-1,-2)], mci.bs[,-1], nl.bs[,-1])
-  dat.m6 <- rbind(ad.m6[,-1], mci.m6[,-1], nl.m6[,-1])
-  dat.m12 <- rbind(ad.m12[,-1], mci.m12[,-1], nl.m12[,-1])
-  rid <- c(ad.m12[,1], mci.m12[,1], nl.m12[,1])
+  dat.bs <- rbind(ad.bs[,c(-1,-2)], nl.bs[,-1])
+  dat.m6 <- rbind(ad.m6[,-1], nl.m6[,-1])
+  dat.m12 <- rbind(ad.m12[,-1], nl.m12[,-1])
+  rid <- c(ad.m12[,1], nl.m12[,1])
   # First 90 regions of interest
   dat.bs <- dat.bs[,1:90]
   dat.m6 <- dat.m6[,1:90]
@@ -36,7 +36,6 @@ load(file = "./ADNI.RData")
 #######################################################################
 #               Testing & Training Set Construction  
 #######################################################################
-num.break <- c(nrow(ad.bs), nrow(ad.bs) + nrow(mci.bs), nrow(ad.bs) + nrow(mci.bs) + nrow(nl.bs))
 num.sub <- nrow(dat.bs) # num of subjects is 324 
 dat.no <- c(1:num.sub)
 num.epo <- 3 # num of epochs is 3, baseline, m06, m12
@@ -61,145 +60,33 @@ test.m12 <- dat.m12[test.no,]
 #######################################################################
 #                  Computing the coefficients w  
 #######################################################################
-library(quadprog) # this is a quadratic problem
-## using kernel trick
-kernel <- function(x, y, type = "linear") {
-  switch(type,
-         linear     =  x%*%y,
-         gaussian   =  exp(-sum((x-y)^2)/2),
-         polynomial =  (x%*%y+1)^2)
-}
-
-## compute the kernel matrix in the Lagrange dual
-## computeK = k(x2,y2)-k(x1,y1)-k(x2,y1)+k(x1,y1)
-computeK <- function(x1, x2, y1, y2, type = "linear") {
-  return(kernel(x1,y1,type)-kernel(x1,y2,type)-kernel(x2,y1,type)+kernel(x2,y2,type))  
-}
-
-## merge dataset by observations
-merge.all <- function(x, ..., by = "row.names") {
-  L <- list(...)
-  for (i in seq_along(L)) {
-    x <- merge(x, L[[i]], by = by)
-    rownames(x) <- x$Row.names
-    x$Row.names <- NULL
-  }
-  return(x)
-}
-
-## construct D
-Kmat <- function(data, m, n, num2, type = "linear") {
-  num <- sum(n-1)
-  Dmat <- matrix(NA, nrow = num, ncol = num)
-  for(i in 1:num) {
-    x.i <- (i+1)%/%2 # current subject
-    x.j <- i%%2    # current time
-    if(x.j > 0) {
-      x1 <- data[x.i, 1:num2]; x2 <- data[x.i, (num2+1):(2*num2)]
-    } else {
-      x1 <- data[x.i, (num2+1):(2*num2)]; x2 <- data[x.i, (2*num2+1):(3*num2)]
-    }
-    for(j in 1:i) {
-      y.i <- (j+1)%/%2
-      y.j <- j%%2
-      if(y.j > 0) {
-        y1 <- data[y.i, 1:num2]; y2 <- data[y.i, (num2+1):(2*num2)]
-      } else {
-        y1 <- data[y.i, (num2+1):(2*num2)]; y2 <- data[y.i, (2*num2+1):(3*num2)]
-      }
-      Dmat[i,j] <- computeK(x1, x2, y1, y2, type)
-      Dmat[j,i] <- Dmat[i,j]
-    }
-  }
-  return(Dmat)
-}
-
-m <- nrow(train.bs)
-n <- rep(3, m)
-num <- sum(n-1)
-num2 <- ncol(train.bs)
-C <- -1
+source(file = "./R/HealthIndex.R")
+type = "polynomial"
 train.all <- merge.all(train.bs, train.m6, train.m12)
-
-solveHI <- function(data, num.sub, num.var, num.periods, type = "linear") {
-  Dmat <- Kmat(as.matrix(train.all), m, n, num2, "linear") 
-  dvec <- rep(1, num)
-  bvec <- c(rep(0, num), rep(C, num))
-  Amat <- rbind(diag(num), -diag(num))
-  
-  # find nearest positive definite matrix since Dmat is not always PD
-  require(Matrix)
-  Dmat_PD <- as.matrix(nearPD(Dmat)$mat)
-  
-  result <- solve.QP(Dmat = Dmat_PD, dvec = dvec, Amat = t(Amat), bvec = bvec)
-  vars <- result$solution
-  return(vars)  
-}
-
-w <- solveHI()
-
-computeHI <- function(train, test, coef, type = "linear") {
-  num.test <- nrow(test)    
-  for (i in 1:num.test) {
-    HI <- test[i,]
-    for (j in 1:218) {
-      kernel(train.all[j,91:180], HI, type) - kernel(train.all[j,1:90], HI, type) 
-    }
-  }
-}
+coef <- solveHI(train.all, nrow(train.all), 3, 90, -1, type)
 
 #######################################################################
 #                         Health Index Construction
 #######################################################################
-ind.bs <- as.matrix(test.bs) %*% omega$V1
-ind.m6 <- as.matrix(test.m6) %*% omega$V1
-ind.m12 <- as.matrix(test.m12) %*% omega$V1
+ind.bs <- apply(test.bs, 1, 
+                function(x) computeHI(train.all, x, coef, nrow(train.all), 3, 90, type))
+ind.m6 <- apply(test.m6, 1,
+                function(x) computeHI(train.all, x, coef, nrow(train.all), 3, 90, type))
+ind.m12 <- apply(test.m12, 1,
+                function(x) computeHI(train.all, x, coef, nrow(train.all), 3, 90, type))
 
-err.vec <- 1 - c(ind.m6[,1] - ind.bs[,1], ind.m12[,1] - ind.m6[,1])
-err <- sum(err.vec[err.vec > 0])
-
-test.sub <- nrow(test.bs)
-dat <- as.data.frame(cbind(c(1:test.sub), ind.bs, ind.m6, ind.m12))
-# add group info, AD, MCI, NI
-dat$group = NA;
-dat[which(test.no <= num.break[3]),]$group = "NC"
-dat[which(test.no <= num.break[2]),]$group = "MCI"
-dat[which(test.no <= num.break[1]),]$group <- "AD"
-as.factor(dat$group)
-
-write.csv(cbind(rid = rid[test.no], dat[,-1]), file = "./data/tmp/idx.csv")
+num.test <- nrow(test.bs)
+dat <- as.data.frame(cbind(c(1:num.test), ind.bs, ind.m6, ind.m12))
 
 #######################################################################
 #                         Visualization
 #######################################################################
 library(ggplot2)
+num.ad <- nrow(ad.bs)
+num.nl <- nrow(nl.bs)
 # Full plot
-ggplot(data = dat, aes(y = V1, x = val, shape = group, color = val2)) + 
-  geom_point(aes(x = V2, shape = group, color = "00")) + 
-  geom_point(aes(x = V3, shape = group, color = "06")) + 
-  geom_point(aes(x = V4, shape = group, color = "12")) + 
-  scale_color_manual(values = c("#FFCC00", "#0033CC", "#CC0033"), breaks = c("00", "06", "12"), 
-                     labels = c("Baseline", "M06", "M12"), name = "time") +
-  scale_x_continuous(name="Health Index Value") +
-  scale_y_continuous(name="Subject No.", breaks = c(1:nrow(dat))) +
-  ggtitle("Health Index on Testing Set")
-
-# Less subjects, clearer plot
-dat2 <- dat[sample(c(1:test.sub), 5),] # for clear visualization
-ggplot(data = dat2) + geom_point(aes(y = V1, x = V2), colour = "#FFCC00", size = 10) + 
-  geom_point(aes(y = V1, x = V3), colour = "#0033CC", size = 10) + 
-  geom_point(aes(y = V1, x = V4), colour = "#CC0033", size = 10) + 
-  scale_x_continuous(name="Health Index Value") +
-  scale_y_continuous(name="Subject No.") +
-  ggtitle("Health Index, Yellow = baseline, Blue = m06, Red = m12")
-
-#######################################################################
-#                         Other Visualization   
-#######################################################################
-omega.name <- omega.name$V2
-viz.omega <- cbind(omega, name = omega.name[1:90])
-viz.omega$no <- c(1:length(omega$V1))
-ggplot(data = viz.omega, aes(y = V1, x = no)) + geom_bar(stat = "identity") +
-  scale_x_discrete(breaks = viz.omega$no, labels = omega.name[1:90]) +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1), axis.title.x = element_blank()) +
-  ylab("Relative Importance")
+ggplot(data = dat, aes(y = V1)) + 
+  geom_point(aes(x = ind.bs), color = "green") + 
+  geom_point(aes(x = ind.m6), color = "yellow") + 
+  geom_point(aes(x = ind.m12), color = "red") +
+  geom_hline(yintercept = 26)
